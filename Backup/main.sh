b14a4153f7082a680554473e8712bdd3e711bd2b
Armands Dunskis <armands.dunskis2@mail.dcu.ie>
2023-03-23 21:36:11 +0000

    Added shifting to next arg after options

--- Backup/main.sh
+++ Backup/main.sh
@@ -5,7 +5,21 @@ usage(){
 }
 
 backup(){
-  files=$@
+  local d;
+  while getopts "d:" option; # TODO: Add error handling
+  do
+    case "${option}" in
+      d)
+        echo "option: d"
+        ;;
+      *)
+        echo "option: None"
+        ;;
+    esac
+  done
+  shift $((OPTIND-1)) # Shift to position of next arg after options
+
+  files=$*
   for file in $files
   do
     echo "$file" "$file.bak"
